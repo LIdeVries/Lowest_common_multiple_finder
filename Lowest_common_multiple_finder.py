0e8cb524d733b1023361b0f0b145995170be8f44
LIdeVries <87062099+LIdeVries@users.noreply.github.com>
2021-10-28 19:04:28 +0200

    Add files via upload

---
+++ Lowest_common_multiple_finder.py
@@ -0,0 +1,61 @@
+#Create a set of all numbers in list. 
+
+#use compare below to look for those numbers and count their instances..
+
+from typing import Counter
+
+
+def compare(counta): # looking through each list for ascending numbers
+    sety = set(counta)
+    return sety
+
+toto = []
+sety = {}
+def totals(counter):
+    global toto
+    toto.append(counter)
+    
+def form(listy):
+    global sety
+    global counter
+    counter=[]
+    factor = 2
+    counta = []
+    for number in listy:
+        while number !=1:
+            if number % factor == 0:
+                number = number/factor
+                counter.append(factor)
+                counta.append(factor)
+                factor = 2
+            else:
+                factor += 1       
+        totals(counter)
+        sety = compare(counta) 
+        counter = []
+        
+
+#input Numbers and terminate with #
+listy =[]
+inp = input('Enter numbers, terminate with #\n>> ')
+if inp != '#':
+    listy.append(int(inp))
+while inp != "#":
+    inp = input('Enter numbers, terminate with #\n>> ')
+    if inp != '#':
+        listy.append(int(inp))
+
+#Send to formula and print
+form (listy)
+
+total = 1
+for num in sety: #from here fix
+    scount = 0 
+    for subl in toto:
+        s2count = subl.count(num)
+        if s2count > scount:
+                scount = s2count
+    if scount > 0:
+        total = total*(num**scount)
+
+print ('The LCM of',sety,'is',total)
